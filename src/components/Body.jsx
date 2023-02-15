e7e16033ad9b6b846352d2e4bea361f3152c76bc
ThilankaMORA <wickramawmtb.19@uom.lk>
2023-02-15 16:48:59 +0530

    start

---
+++ src/components/Body.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Body extends Component {
+    //state = {  } 
+    render() { 
+        return (<h1>Hello World</h1>);
+    }
+}
+ 
+export default Body;
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+class Header extends Component {
+    //state = {  } 
+    render() { 
+        return (
+        <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+            <Container>
+            <Navbar.Brand href="#home">React ROS Robot</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="#/About">About</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+           </Container>   
+        </Navbar>
+      );
+    }
+}
+ 
+export default Header;
