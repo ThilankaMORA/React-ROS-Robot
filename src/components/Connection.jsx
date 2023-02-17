66365027c5fa0dff761388a74adb032a935bde03
ThilankaMORA <wickramawmtb.19@uom.lk>
2023-02-17 14:50:27 +0530

    test

--- src/components/Connection.jsx
+++ src/components/Connection.jsx
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
+import { Alert, Container } from 'react-bootstrap';
 
 class Connection extends Component {
-    state = {  };
+    state = { connected: false, ros:null };
+
+    constructor() {
+        super();
+        this.init_connection();
+    }
+
     init_connection(){
-        var ros = new window.ROSLIB.Ros();
+        this.state.ros = new window.ROSLIB.Ros();
+        console.log(this.state.ros);
+
+        this.state.ros.on("connection", () => {
+            console.log("connection established!");
+            this.setState({connected:true});
+        });
+
+        this.state.ros.on("close", () => {
+            console.log("connection is closed!");
+            this.setState({connected:false});
+        });
     } 
+    
     render() { 
         return (
         <div>
-        <h2>Connection</h2>
+        <Alert className={this.state.connected? "alert-custom-green":"alert-custom-red"}>{this.state.connected? "Robot Connected":"Robot Disconnected !"}</Alert>
         </div>
         );
     }
